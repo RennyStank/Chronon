da71483c920e995300a51666034010c5b10ec4be
RennyStank <71740121+RennyStank@users.noreply.github.com>
2020-12-28 16:44:48 +0800

    Add todo-list

---
+++ src/components/todo-list/todo-list.js
@@ -0,0 +1,17 @@
+function todoAddTask() {
+
+}
+
+function todoDeleteTask() {
+    
+}
+
+var tasks = [];
+
+document.addEventListener("DOMContentLoaded") = function() {
+    if (tasks == []) {
+
+    } else {
+
+    }
+}
