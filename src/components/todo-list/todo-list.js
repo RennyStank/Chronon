8d5446b9e2bd5a7cdcfcaa3c6e5f42bb9f0389cf
RennyStank <71740121+RennyStank@users.noreply.github.com>
2020-12-28 17:35:27 +0800

    Add nullScreen

--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -1,12 +1,22 @@
 function todoAddTask() {
-
+    if (tasks == []) {
+        mainScreen.style.display = 'none';
+        nullScreen.style.display = 'block';
+    }
+    //add task
 }
 
 function todoDeleteTask() {
-    
+    //delete task
+    if (tasks == []) {
+        mainScreen.style.display = 'none';
+        nullScreen.style.display = 'block';
+    }
 }
 
 var tasks = [];
+var nullScreen = document.getElementById("nullScreen");
+var mainScreen = document.getElementById("mainScreen");
 
 document.addEventListener("DOMContentLoaded") = function() {
     if (tasks == []) {
