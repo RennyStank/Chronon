26f8cb949f1553ce65b728798b4651fddb284267
RennyStank <71740121+RennyStank@users.noreply.github.com>
2020-12-30 16:14:08 +0800

    Add task methods

---
+++ main-process/tasks.js
@@ -0,0 +1,17 @@
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+const db = admin.firestore();
+
+
+function addTask(user, index, name, timeframe) {
+    const newTask = {
+        name: name,
+        timeframe: timeframe
+    }
+    const res = await db.collection("users").doc(user).collection("tasks").add(newTask);
+}
+
+function deleteTask(id, user) {
+    const res = await db.collection("users").doc(user). collection("tasks").doc(id).delete();
+}
