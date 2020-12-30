313ced927cc4fe41039c41ae2c9a9ba9abb2eb47
RennyStank <71740121+RennyStank@users.noreply.github.com>
2020-12-30 18:47:41 +0800

    Add task listener

--- main-process/tasks.js
+++ main-process/tasks.js
@@ -3,15 +3,31 @@ admin.initializeApp();
 
 const db = admin.firestore();
 
+const taskRef = db.collection("users").doc(user).collection("tasks");
 
-function addTask(user, name, timeframe) {
+const listener = taskRef.onSnapshot(querySnapshot => {
+    querySnapshot.docChanges().forEach(changes => {
+        if (changes.type === "added") {
+
+        }
+        if (changes.type === "modified") {
+
+        }
+        if (changes.type === "deleted") {
+            
+        }
+    })
+}) 
+
+
+function addTask(name, timeframe) {
     const newTask = {
         name: name,
         timeframe: timeframe
     }
-    const res = await db.collection("users").doc(user).collection("tasks").add(newTask);
+    const res = await taskRef.add(newTask);
 }
 
-function deleteTask(id, user) {
-    const res = await db.collection("users").doc(user).collection("tasks").doc(id).delete();
+function deleteTask(id) {
+    const res = await taskRef.doc(id).delete();
 }
