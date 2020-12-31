01890435304d8f1045e599fd017a274cace96707
RennyStank <71740121+RennyStank@users.noreply.github.com>
2020-12-31 23:27:02 +0800

    Update list-item.tsx

--- src/components/todo-list/react/list-item.tsx
+++ src/components/todo-list/react/list-item.tsx
@@ -1,10 +1,28 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-namespace listItem {
+namespace list {
     export interface Props {
 
     }
 
-    
+    const listItem = () => null;
+    const listGrp = () => null;
+
+    export class TodoListView extends React.Component {
+        constructor(props) {
+            super(props);
+            this.state = {
+
+            }
+        }
+
+        render() {
+            return (
+                <div>
+                    <span></span>
+                </div>
+            )
+        }
+    }
 }
